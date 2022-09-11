6354594c23101ddace7d4256e531be912c73c21d
rudro54 <rudro54@gmail.com>
2022-09-11 18:36:25 +0600

    learning running

--- introduction-to-javascript/array.js
+++ introduction-to-javascript/array.js
@@ -1,3 +1,14 @@
-var friendsAge = [23, 24, 25, 26, 28];
-console.log(friendsAge);
-console.log(friendsAge.length);
+// var friendsAge = [23, 24, 25, 26, 28];
+// console.log(friendsAge);
+// console.log(friendsAge.length);
+// var indexNumber = friendsAge.indexOf(25);
+// console.log(indexNumber);
+// var indexNumber2 = friendsAge.indexOf(100);
+// console.log(indexNumber2);
+
+var friends = ['balam', 'kalam', 'salam'];
+
+friends.push('gelam');
+
+friends.push('pailam', 'harailam');
+console.log(friends);
