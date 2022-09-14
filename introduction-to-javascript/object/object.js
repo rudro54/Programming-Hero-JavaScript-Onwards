4a7e1d7c0476614697cd441995a1ab0a236e7770
rudro54 <rudro54@gmail.com>
2022-09-15 01:03:09 +0600

    object added

---
+++ introduction-to-javascript/object/object.js
@@ -0,0 +1,27 @@
+var student = {
+
+    id: 115,
+    name: 'solaiman khan',
+    class: 9,
+    marks: 98
+
+
+}
+
+var mobile = {
+
+    name: 'samsung',
+    price: 19000,
+    storage: '64gb',
+    camera: '7MP'
+
+}
+
+var myComputer = {
+    name: 'lenovo',
+    model: 'thinkpad',
+    price: 100000,
+    processor: 'i12'
+}
+
+console.log(myComputer.price);
