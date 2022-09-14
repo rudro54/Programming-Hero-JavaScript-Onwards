0be6cfcfd98d89bd8101ee1a3ebeafee6305c778
rudro54 <rudro54@gmail.com>
2022-09-14 22:32:37 +0600

    running

---
+++ introduction-to-javascript/functions/functions.js
@@ -0,0 +1,13 @@
+
+// function declaration
+
+function startFan() {
+    console.log('stand up');
+    console.log('walks towards the switch');
+    console.log('press the swithch');
+
+}
+
+// calling the function
+
+startFan();
