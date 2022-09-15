f538831ee7fff0660d4041572964c2d657f828a4
rudro54 <rudro54@gmail.com>
2022-09-15 14:58:30 +0600

    core concept recap

---
+++ introduction-to-javascript/apply-js-core-concepts/inchToFeet.js
@@ -0,0 +1,11 @@
+function inchToFeet(inches) {
+    const feet = inches / 12;
+    return feet;
+}
+const dadaInches = 144;
+const dadaFeet = inchToFeet(dadaInches);
+console.log('Dada Feet :', dadaFeet);
+
+const nanaInches = 168;
+const nanaFeet = inchToFeet(nanaInches);
+console.log('Nana Feet :', nanaFeet);
