2490ba6b93529e5524ba6ccc145430b9d11acc55
rudro54 <rudro54@gmail.com>
2022-09-18 23:11:47 +0600

    some problem solving

---
+++ introduction-to-javascript/Array/totalShoppingCart.js
@@ -0,0 +1,22 @@
+// array of objects 
+
+const shoppingCart = [
+    { name: 'shoe', price: 1200, quantity: 2 },
+    { name: 'shirt', price: 2200, quantity: 5 },
+    { name: 'pant', price: 3700, quantity: 4 },
+    { name: 'belt', price: 600, quantity: 3 },
+    { name: 'sunglass', price: 5000, quantity: 2 }
+];
+
+function totalCost(products) {
+    let sum = 0;
+    for (let i = 0; i < products.length; i++) {
+        const product = products[i];
+        const productTotal = product.price * product.quantity;
+        sum = sum + productTotal;
+    }
+    return sum;
+}
+
+const expense = totalCost(shoppingCart);
+console.log('the total cost is :', expense);
