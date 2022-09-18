e8b87d98ef67bb5b86d93ff04cbe2b80a2d0e314
rudro54 <rudro54@gmail.com>
2022-09-18 14:29:44 +0600

    some problem solving

--- introduction-to-javascript/Array/slice.js
+++ introduction-to-javascript/Array/slice.js
@@ -3,6 +3,6 @@ const partial = friends.slice(2, 5);
 console.log(partial);
 
 const moreFriends = [12, 45, 32, 22, 44, 62, 29, 69, 87];
-const partialSplice = moreFriends.splice(2, 5);
+const partialSplice = moreFriends.splice(2, 5, 99, 555, 7777);
 console.log(partialSplice);
 console.log(moreFriends);
