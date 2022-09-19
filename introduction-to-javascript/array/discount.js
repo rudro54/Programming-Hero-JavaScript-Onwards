f3ef1d0f10466092477d7a1700fc05552b1c47d8
rudro54 <rudro54@gmail.com>
2022-09-19 08:59:43 +0600

    updated

---
+++ introduction-to-javascript/array/discount.js
@@ -0,0 +1,15 @@
+/*  
+if ticket number is less than 100 -> per ticket price is 100 taka per pcs
+
+if ticket number is more than 100 but less than 200 -> 
+first 100 ticket 100 taka per pcs
+rest 90 taka per pcs
+
+if more than 200 tickets -> 
+first 100 -> 100 taka per pcs
+101-200 -> 90 taka per pcs
+200+ 70 taka per pcs
+
+
+
+*/
