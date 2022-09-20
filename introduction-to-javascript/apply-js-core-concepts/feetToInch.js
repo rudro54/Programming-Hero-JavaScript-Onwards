6c78371b654a88727ce43c547cd00fc9214e04d7
rudro54 <rudro54@gmail.com>
2022-09-20 17:08:12 +0600

    some problem solving

---
+++ introduction-to-javascript/apply-js-core-concepts/feetToInch.js
@@ -0,0 +1,7 @@
+function feetToInch(feetAmount) {
+    let result = feetAmount * 12;
+    return result;
+}
+
+let amountInInch = feetToInch(12);
+console.log("Your desired inch is :", amountInInch + ' inch'); 
