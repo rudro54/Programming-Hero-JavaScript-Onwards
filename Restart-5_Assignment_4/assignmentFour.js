1f0ca49047ccdc5fad4599926a60cb35a99b4dee
rudro54 <rudro54@gmail.com>
2022-09-22 01:50:32 +0600

    assignment-4: all done now doing validation

--- Restart-5_Assignment_4/assignmentFour.js
+++ Restart-5_Assignment_4/assignmentFour.js
@@ -2,7 +2,7 @@
 // Answer to question number one 
 
 function radianToDegree(radianValue) {
-    if (isNaN(radianValue) || typeof radianValue !== 'number') {
+    if (isNaN(radianValue)) {
         throw new Error('your parameter must be a numeric one');
     } // validation if the parameter is taking numeric value or not
 
@@ -11,12 +11,15 @@ function radianToDegree(radianValue) {
     return degreeValue.toFixed(2); // used toFixed() for setting only 2 digits after decimal point.
 
 }
-console.log(radianToDegree('54'));
+
 
 // answer to question number two 
 
 function isJavaScriptFile(fileName) {
 
+    if (typeof fileName !== 'string') { // to check if a string is passed or not
+        throw new Error('your parameter must be a string type');
+    }
 
     let fileTypeCheck = fileName.endsWith('.js'); // checking the extension
     if (fileTypeCheck === true) {
@@ -28,9 +31,18 @@ function isJavaScriptFile(fileName) {
 }
 
 
+
 // answer to question number three 
 
 function oilPrice(dieseltQuantity, petrolQuantity, octaneQuantity) {
+
+    if (arguments.length != 3) {  // to check if 3 arguments are passed correctly
+        throw new Error('You must provide three separate parameters');
+    }
+    else if (isNaN(dieseltQuantity) || isNaN(petrolQuantity) || isNaN(octaneQuantity)) {
+        throw new Error('All of your parameters must be numeric ones');
+    }
+
 
 
     let dieselAmount = dieseltQuantity * 114; // diesel price 114 taka per unit
@@ -42,9 +54,16 @@ function oilPrice(dieseltQuantity, petrolQuantity, octaneQuantity) {
 
 }
 
+
+
 // answer to question number four 
 
 function publicBusFare(totalPassengers) {
+
+    if (isNaN(totalPassengers)) {
+        throw new Error('your parameter must be a numeric one');
+    } // validation if the parameter is taking numeric value or not
+
     let restPeopleAfterPrivateBus = totalPassengers % 50; // remainder of 50 person per each bus goers 
     return (restPeopleAfterPrivateBus % 11) * 250;  // remainder of 11 person per each micro-goers times fare
 }
@@ -55,6 +74,14 @@ function publicBusFare(totalPassengers) {
 
 function isBestFriend(firstFriend, secondFriend) {
 
+    if (arguments.length != 2) {  // to check if 2 arguments are passed correctly
+        throw new Error('You must provide two separate parameters');
+    }
+    else if (typeof firstFriend !== Object && typeof secondFriend !== Object) {
+        throw new Error('You must provide two separate objects'); // to check if 2 arguments are objects 
+    }
+
+
     if ((firstFriend.name === secondFriend.friend) && (firstFriend.friend === secondFriend.name)) {
         return true;
     }
@@ -64,6 +91,8 @@ function isBestFriend(firstFriend, secondFriend) {
 
 
 }
+
+
 
 
 
