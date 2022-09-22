b77a1d688cacacdb9d5bb762eaff0e0a34e1f12c
rudro54 <rudro54@gmail.com>
2022-09-22 22:31:42 +0600

    recurssion folder name change

--- introduction-to-javascript/recursionAndAdvancedJS/arguments.js
+++ introduction-to-javascript/recursionAndAdvancedJS/arguments.js
@@ -1,5 +1,6 @@
 function add(num1, num2) {
     console.log(num1, num2);
     console.log(arguments);
+    console.log(arguments[3]) // showing the result as well 33
 }
 add(12, 13, 32, 33, 22, 11);
