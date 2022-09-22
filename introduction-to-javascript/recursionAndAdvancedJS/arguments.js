11bf1a2929c73a86f8fb0a303c082cfb0d90ab58
rudro54 <rudro54@gmail.com>
2022-09-22 22:29:52 +0600

    recurssion folder name change

---
+++ introduction-to-javascript/recursionAndAdvancedJS/arguments.js
@@ -0,0 +1,5 @@
+function add(num1, num2) {
+    console.log(num1, num2);
+    console.log(arguments);
+}
+add(12, 13, 32, 33, 22, 11);
---
+++ introduction-to-javascript/recursionAndAdvancedJS/array.js
@@ -0,0 +1,9 @@
+const products = [
+
+    { name: 'phone', price: '22000' },
+    { name: 'laptop', price: '32000' }
+
+];
+
+console.log(products[1]);
+//products[1];
