d97aa5ad88dc92ff570fafb98b8097a04cce2b92
rudro54 <rudro54@gmail.com>
2022-09-24 17:48:02 +0600

    dom events

--- introduction-to-javascript/jsDomEvents/js/events.js
+++ introduction-to-javascript/jsDomEvents/js/events.js
@@ -1,5 +1,8 @@
 console.log('this is a separate js file');
 
+
+// option :1 directly set on the html element 
+
 // option 2 : add onclick function 
 
 function makeRed() {
@@ -15,6 +18,13 @@ makePinkButton.onclick = makePink; // no functiuon call here just write the name
 
 function makePink() {
     document.body.style.backgroundColor = 'pink';
+}
+
+// option 3 : b
+
+const makePurpleButton = document.getElementById('make-purple');
+makePurpleButton.onclick = function makePurple() {
+    document.body.style.backgroundColor = 'purple';
 }
 
 
