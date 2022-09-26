bcd476b1de633238c6d1d0879b792861de571c63
rudro54 <rudro54@gmail.com>
2022-09-26 15:13:35 +0600

    dom events-baap-er-bank js file added and running

---
+++ introduction-to-javascript/baap-er-bank/js/login.js
@@ -0,0 +1,8 @@
+
+// step 1 : add click event handler with the submit button
+
+document.getElementById('btn-submit').addEventListener('click', function () {
+    console.log('submit button clicked');
+});
+
+// step 2 : get the email address inside the email input field
