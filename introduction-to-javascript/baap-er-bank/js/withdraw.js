38278dff926b7e32e18a3047cb453097a4034edc
rudro54 <rudro54@gmail.com>
2022-09-26 19:29:33 +0600

    dom events-baap-er-bank coding running

---
+++ introduction-to-javascript/baap-er-bank/js/withdraw.js
@@ -0,0 +1,51 @@
+/*
+1. add event handler with the withdraw button 
+2. get the withdraw amount from the withdraw  input field 
+2-5 also make sure to convert the input into a number by using parsefloat
+3. get previous withdraw total
+4. calculate total withdraw amount
+4-5. set total withdraw amount 
+5. get the previous balance total
+7. clear the input field
+
+
+*/
+
+
+
+
+// step 1 
+
+document.getElementById('btn-withdraw').addEventListener('click', function () {
+
+    // step 2
+
+    const withdrawField = document.getElementById('withdraw-field');
+    const newWithdrawFieldAmountString = withdrawField.value;
+    const newWithdrawAmount = parseFloat(newWithdrawFieldAmountString);
+
+    // step 3 
+
+    const withdrawTotalElement = document.getElementById('withdraw-total');
+    const previousWithdrawTotalString = withdrawTotalElement.innerText;
+    const previousWithdrawTotal = parseFloat(previousWithdrawTotalString);
+
+    // step 4
+
+    const currentWithdrawTotal = previousWithdrawTotal + newWithdrawAmount;
+    withdrawTotalElement.innerText = currentWithdrawTotal;
+
+    // step 5
+
+    const balanceTotalElement = document.getElementById('balance-total');
+    const previousBalanceTotalElementString = balanceTotalElement.innerText;
+    const previousBalanceTotal = parseFloat(previousBalanceTotalElementString);
+
+
+
+    // step 7
+
+    withdrawField.value = ' ';
+
+
+});
