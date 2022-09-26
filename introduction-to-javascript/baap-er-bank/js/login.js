d737da95edb6b9220eba56915f1e5ad7b105b98e
rudro54 <rudro54@gmail.com>
2022-09-26 16:13:30 +0600

    dom events-baap-er-bank js file added and running

--- introduction-to-javascript/baap-er-bank/js/login.js
+++ introduction-to-javascript/baap-er-bank/js/login.js
@@ -2,7 +2,33 @@
 // step 1 : add click event handler with the submit button
 
 document.getElementById('btn-submit').addEventListener('click', function () {
-    console.log('submit button clicked');
+
+    // step 2 : get the email address inside the email input field
+    // always remember to use .value to get text from an input field
+
+    const emailField = document.getElementById('user-email');
+    const email = emailField.value;
+
+    // step 3 : get passowrd
+    // 3 a : set id on the html element 
+    // 3 b : get the element
+    // 3 c : get the value from the elemenet 
+
+    const passwordField = document.getElementById('user-password');
+    const passowrd = passwordField.value;
+
+    // Danger : do not compare or verify email password on the client side, this will got to server side  
+    // we will learn that later but for learning we are doin this right now. 
+
+    // stp 4 : verify email and password 
+
+    if (email === 'sontan@baap.com' && passowrd === 'secret') {
+        console.log('valid user');
+    }
+    else {
+        console.log('invalid user');
+    }
+
 });
 
-// step 2 : get the email address inside the email input field
+
