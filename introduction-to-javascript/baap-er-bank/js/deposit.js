944268a71ba17ad8c68ff80984e5425379b0ed77
rudro54 <rudro54@gmail.com>
2022-09-26 19:02:26 +0600

    dom events-baap-er-bank coding running

--- introduction-to-javascript/baap-er-bank/js/deposit.js
+++ introduction-to-javascript/baap-er-bank/js/deposit.js
@@ -7,6 +7,33 @@ document.getElementById('btn-deposit').addEventListener('click', function () {
     // for input field use .value to get the value from the input field
 
     const depositField = document.getElementById('deposit-field');
-    const depositAmount = depositField.value;
-    console.log(depositAmount);
+    const newDepositAmountString = depositField.value;
+    const newDepositAmount = parseFloat(newDepositAmountString);
+
+    // step 3 : get the current deposit total amount 
+    // for non input element ( element other than  input textarea ) use innerText to
+    // get the text
+
+    const depositTotalElement = document.getElementById('deposit-total');
+    const previousDepositTotalString = depositTotalElement.innerText;
+    const previousDepositTotal = parseFloat(previousDepositTotalString);
+
+    // step 4 : add numbers to set the total deposit
+    const currentDepositTotal = previousDepositTotal + newDepositAmount;
+    depositTotalElement.innerText = currentDepositTotal;
+
+    // step 5 : get current balance total 
+    const balanceTotalElement = document.getElementById('balance-total');
+    const previousBalanceTotalElementString = balanceTotalElement.innerText;
+    const previousBalanceTotal = parseFloat(previousBalanceTotalElementString);
+
+    // step 6 : calculate current total balance 
+    const currentBalanceTotal = previousBalanceTotal + newDepositAmount;
+    // set the balance total
+    balanceTotalElement.innerText = currentBalanceTotal;
+
+    // step : clear the deposit field 
+    depositField.value = ' ';
+
+
 });
