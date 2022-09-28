b7c1fad0adfa052be4c846f20eb4a4fd43afc41e
rudro54 <rudro54@gmail.com>
2022-09-28 16:35:04 +0600

    functional baap-er-bank running

--- introduction-to-javascript/functional-baap-er-bank/js/deposit.js
+++ introduction-to-javascript/functional-baap-er-bank/js/deposit.js
@@ -1,5 +1,15 @@
 document.getElementById('btn-deposit').addEventListener('click', function () {
 
+    const newDepositValue = getInputTextValue('deposit-amount');
+    const oldDepositvalue = getDisplayElementValue('deposit-total');
+    const totalDepositValue = newDepositValue + oldDepositvalue;
+    setDisplayElementValue('deposit-total', totalDepositValue);
+
+
+    const oldBalanceValue = getDisplayElementValue('balance-total');
+    const newBalanceValue = getDisplayElementValue('deposit-total');
+    const totalBalanceValue = oldBalanceValue + newBalanceValue;
+    setDisplayElementValue('balance-total', totalBalanceValue);
 
 
 
--- introduction-to-javascript/functional-baap-er-bank/js/utilities.js
+++ introduction-to-javascript/functional-baap-er-bank/js/utilities.js
@@ -1,15 +1,23 @@
 
-function getInputTextValue(inputText) {
-    const inputText = document.getElementById('inputText');
+function getInputTextValue(inputTextId) {
+    const inputText = document.getElementById(inputTextId);
     const inputTextString = inputText.value;
     const inputTextConverted = parseFloat(inputTextString);
+    inputText.value = '';
     return inputTextConverted;
+
 }
 
-function getDisplayTextValue(inputText) {
-    const displayText = document.getElementById('inputText');
-    const displayTextString = displayText.innerText;
-    const displayTextConverted = parseFloat(displayTextString);
-    return displayTextConverted;
+function getDisplayElementValue(inputElement) {
+    const displayElement = document.getElementById(inputElement);
+    const displayElementString = displayElement.innerText;
+    const displayElementConverted = parseFloat(displayElementString);
+    return displayElementConverted;
+}
+
+function setDisplayElementValue(inputElement, valueToSet) {
+    const displayElement = document.getElementById(inputElement);
+    displayElement.innerText = valueToSet;
+
 }
 
