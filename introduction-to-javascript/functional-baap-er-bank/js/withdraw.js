e742370da864f51f80e6aae4dcecd83bf1815879
rudro54 <rudro54@gmail.com>
2022-09-28 16:52:18 +0600

    functional baap-er-bank complete

--- introduction-to-javascript/functional-baap-er-bank/js/withdraw.js
+++ introduction-to-javascript/functional-baap-er-bank/js/withdraw.js
@@ -1,6 +1,14 @@
 document.getElementById('btn-withdraw').addEventListener('click', function () {
 
+    const oldWithdrawValue = getDisplayElementValue('withdraw-total');
+    const newWithdrawValue = getInputTextValue('withdraw-amount');
+    const totalWithdrawValue = oldWithdrawValue + newWithdrawValue;
 
+    setDisplayElementValue('withdraw-total', totalWithdrawValue);
+
+    const oldBalanceValue = getDisplayElementValue('balance-total');
+    const newBalanceValue = oldBalanceValue - newWithdrawValue;
+    setDisplayElementValue('balance-total', newBalanceValue);
 
 
 
