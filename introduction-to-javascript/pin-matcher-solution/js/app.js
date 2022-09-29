78cb44abe54aa78ec8c2b2c9461f45caa4abd5cc
rudro54 <rudro54@gmail.com>
2022-09-30 01:18:22 +0600

    pin-matcher-added

--- introduction-to-javascript/pin-matcher-solution/js/app.js
+++ introduction-to-javascript/pin-matcher-solution/js/app.js
@@ -24,6 +24,35 @@ document.getElementById('generate-pin').addEventListener('click', function () {
     const displayPinField = document.getElementById('display-pin');
     displayPinField.value = pin;
 
+});
+
+document.getElementById('calculator'); addEventListener('click', function (event) {
+    const number = event.target.innerText;
+    const typedNumberField = document.getElementById('typed-numbers');
+    const previousTypedNumber = typedNumberField.value;
+
+    if (isNaN(number)) {
+        if (number === 'C') {
+            typedNumberField.value = '';
+        }
+        else if (number === '<') {
+            const digits = previousTypedNumber.split('');
+            digits.pop();
+            const remainingDigits = digits.join('');
+            typedNumberField.value = remainingDigits;
+
+        }
+
+    }
+    else {
+
+
+        const newTypedNumber = previousTypedNumber + number;
+        typedNumberField.value = newTypedNumber;
+    }
+});
+
+document.getElementById('verify-pin').addEventListener('click', function () {
 
 
 });
