015beae731a7360e17ad11cc73fafa74a241810c
rudro54 <rudro54@gmail.com>
2022-09-29 14:54:36 +0600

    shopping-cart coding running

--- introduction-to-javascript/shopping-cart-solution/js/case.js
+++ introduction-to-javascript/shopping-cart-solution/js/case.js
@@ -16,21 +16,27 @@ function updateCaseNumber(isIncrease) {
 
     caseNumberField.value = newCaseNumber;
 
+    return newCaseNumber;
+}
 
+function updateCaseTotalPrice(newCaseNumber) {
+    const caseTotalPrice = newCaseNumber * 59;
+    const caseTotalElement = document.getElementById('case-total');
+    caseTotalElement.innerText = caseTotalPrice;
 }
 
 
-
-
-
-
 document.getElementById('btn-case-plus').addEventListener('click', function () {
-    updateCaseNumber(true);
+    const newCaseNumber = updateCaseNumber(true);
+    updateCaseTotalPrice(newCaseNumber);
+
 });
 
 
 document.getElementById('btn-case-minus').addEventListener('click', function () {
-    updateCaseNumber(false);
+    const newCaseNumber = updateCaseNumber(false);
+    updateCaseTotalPrice(newCaseNumber);
+
 
 });
 
