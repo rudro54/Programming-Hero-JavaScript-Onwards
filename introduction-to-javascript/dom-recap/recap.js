3e3b3008e963ceb1c9f1028e4843b0fc1c7fbb99
rudro54 <rudro54@gmail.com>
2022-10-03 23:41:39 +0600

    js recap

---
+++ introduction-to-javascript/dom-recap/recap.js
@@ -0,0 +1,33 @@
+/*
+
+
+
+ 1. how to use javascript with html and css
+ 2. get element or elements from dom
+    2.1 getElementById
+    2.2 getElementByClassName
+    2.3 querySelector
+    2.4 querySelectorAll
+
+     document.quertySelectorAll('.user-container  .disable h3');
+     document.quertySelectorAll ('.myUsers li');
+
+ 3. dynamically add element to the dom document.createElement
+    3.1 set value in the innterHTML
+
+ 4. get value from a dom element 
+    4.1 use innerText ( h1 p h5 div span strong )
+    4.2 use value ( input textarea )
+
+ 5. add eventListener or eventhandler 
+    5.1 click focus blur onchange keyup-> most important 
+    5.2 addEventListener onClick =" "
+   
+ 6. use function to reduce duplicate code
+ 7. event bubble and event deligate 
+
+ 8.
+
+
+
+*/
