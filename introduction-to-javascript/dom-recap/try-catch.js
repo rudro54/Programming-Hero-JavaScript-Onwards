6c7568245cad7ed3405edb670d93454102d9b210
rudro54 <rudro54@gmail.com>
2022-10-04 12:35:24 +0600

    js recap

---
+++ introduction-to-javascript/dom-recap/try-catch.js
@@ -0,0 +1,20 @@
+const day = 'din the day';
+console.log(day);
+console.log(25);
+try {
+    console.log('inside try')
+    // console.log(district);
+}
+catch (error) {
+    console.log(error);
+}
+finally {
+    console.log(100);
+    console.log(100);
+}
+
+
+
+
+
+
