6cea6285cd965f1b6c6ddca4f82a51dabd88af1e
rudro54 <rudro54@gmail.com>
2022-10-04 13:33:39 +0600

    js recap

--- introduction-to-javascript/dom-recap/date.js
+++ introduction-to-javascript/dom-recap/date.js
@@ -1,2 +1,14 @@
 const today = new Date();
 console.log(today);
+
+const date1 = new Date('1971-3-26');
+const date2 = new Date('1971-12-16');
+
+
+if (date1.getTime() < date2.getTime()) {  // the  logic is getTime()starts from january 1970 so this calcultion 
+    // comes such logic that the less time in miliseconds are the oldest dates 
+    console.log('march comes before december');
+}
+else {
+    console.log('december comes after march');
+}
