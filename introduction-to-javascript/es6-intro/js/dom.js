acf8aaf1a384e5593662a190b8e3d26f26aee5ab
rudro54 <rudro54@gmail.com>
2022-10-06 13:18:25 +0600

    es6 intro added and working

---
+++ introduction-to-javascript/es6-intro/js/dom.js
@@ -0,0 +1,29 @@
+document.getElementById('apply-bg').addEventListener("click", function () {
+
+    const friends = document.getElementsByClassName('friend');
+
+    for (const friend of friends) {
+        friend.style.backgroundColor = 'lightblue';
+    }
+
+});
+
+document.getElementById('center-third').addEventListener('click', function () {
+
+    const third = document.getElementById('third-friend');
+    third.style.textAlign = 'center';
+});
+
+document.getElementById('add-friend').addEventListener('click', function () {
+
+    const friendContainer = document.getElementById('friends');
+    const friend = document.createElement('div');
+    friend.innerHTML = `
+    <h3 class="friend-name">New Friend</h3>
+    <p>New friend added</p>
+    
+    `;
+
+    friendContainer.appendChild(friend);
+
+});
