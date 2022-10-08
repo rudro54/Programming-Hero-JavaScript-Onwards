abab4db8cc5e6db37ffc4095a39a793dda623abf
rudro54 <rudro54@gmail.com>
2022-10-08 14:42:21 +0600

    es6 intro added and working

---
+++ introduction-to-javascript/es6-intro/js/template.js
@@ -0,0 +1,15 @@
+const multiline = ` first lien
+second line 
+third line
+forth line`;
+
+// console.log(multiline);
+
+
+const a = 30;
+const b = 20;
+
+const nums = [78, 98, 45, 12, 6, 444];
+
+const summary = `sum of ${a} and ${nums.length} is : ${a + b} `;
+console.log(summary);
