daebbfc0b63a0f428559cde728154094b89307c7
rudro54 <rudro54@gmail.com>
2022-10-09 03:04:10 +0600

    es6 intro added and working

---
+++ introduction-to-javascript/more-es6/forEach.js
@@ -0,0 +1,21 @@
+const friends = ['Tom Hanks', 'Tom Cruize', 'Tom Brady', 'Tom Solaiman'];
+// no return for forEach
+
+friends.forEach(freind => console.log(freind));
+
+// multiline forEach
+
+
+
+
+
+
+
+const products = [
+
+    { id: 1, name: 'laptop', price: 45000 },
+    { id: 2, name: 'mobile', price: 80000 },
+    { id: 3, name: 'watch', price: 35000 },
+    { id: 4, name: 'tablet', price: 23000 }
+
+];
