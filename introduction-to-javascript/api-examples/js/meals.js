2ca94195a439264aa69b31673f633a96aee5ba9d
rudro54 <rudro54@gmail.com>
2022-10-12 00:57:38 +0600

    es6 intro added and working

--- introduction-to-javascript/api-examples/js/meals.js
+++ introduction-to-javascript/api-examples/js/meals.js
@@ -8,7 +8,22 @@ const loadMeals = () => {
 const displayMeals = meals => {
     const mealsContainer = document.getElementById('meal-container');
     meals.forEach(meal => {
-        console.log(meal);
+        const mealDiv = document.createElement('div');
+        mealDiv.classList.add('col');
+        mealDiv.innerHTML = `
+
+        <div class="card">
+            <img src="..." class="card-img-top" alt="...">
+        <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">This is a longer card with supporting text below as a natural lead-in
+            to additional content. This content is a little bit longer.</p>
+        </div>
+        </div>
+        
+        `;
+        mealsContainer.appendChild(mealDiv);
+
     });
 }
 loadMeals();
