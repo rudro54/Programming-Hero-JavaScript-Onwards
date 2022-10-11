dcf1c5a1ef3fbaa0e19dca6cff42c0d22a887354
rudro54 <rudro54@gmail.com>
2022-10-11 21:32:51 +0600

    es6 intro added and working

--- introduction-to-javascript/api-examples/js/countries.js
+++ introduction-to-javascript/api-examples/js/countries.js
@@ -13,7 +13,7 @@ const displayCountries = countries => {
 
         const countryDiv = document.createElement('div');
         countryDiv.classList.add('country');
-        console.log(country);
+        //console.log(country);
         countryDiv.innerHTML = `
             <h3>Name : ${country.name.common} </h3>
             <p> Capitl : ${country.capital ? country.capital[0] : 'no capital'} </p>
@@ -28,6 +28,18 @@ const loadCountryDetails = (code) => {
     //https://restcountries.com/v3.1/alpha/{code}
     const url = (`https://restcountries.com/v3.1/alpha/${code}`)// us tick
     //console.log('get country details', code);
-    console.log(url);
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayCountryDetail(data[0]))
 }
+const displayCountryDetail = country => {
+    const countryDetails = document.getElementById('country-detail');
+    countryDetails.innerHTML = `
+      <h2>Details :${country.name.common} </h2>
+      <img src ="${country.flags.png}">
+
+    
+    `;
+}
+
 loadCountries();
