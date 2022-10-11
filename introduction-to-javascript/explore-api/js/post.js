1f851f10b8407234e8f306791443727b7ea09351
rudro54 <rudro54@gmail.com>
2022-10-11 12:36:43 +0600

    es6 intro added and working

---
+++ introduction-to-javascript/explore-api/js/post.js
@@ -0,0 +1,34 @@
+function loadPosts() {
+    const url = 'https://jsonplaceholder.typicode.com/posts';
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayPost(data))
+
+}
+
+function displayPost(data) {
+
+    const postContainer = document.getElementById('post-container');
+
+    for (const post of data) {
+
+        const div = document.createElement('div');
+        div.classList.add('post');
+
+
+        div.innerHTML = `
+        
+         <h4>User - ${post.userId}  </h4>
+         <h5>Post :  ${post.title} </h5>
+         <p> post description : ${post.body} </p>
+        
+        
+        `;
+        postContainer.appendChild(div);
+
+    }
+
+}
+
+loadPosts();
+
