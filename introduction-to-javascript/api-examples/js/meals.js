0bf71be2869f4b6dbaabea8635860487a87c78ff
rudro54 <rudro54@gmail.com>
2022-10-12 17:06:45 +0600

    es6 intro added and working

--- introduction-to-javascript/api-examples/js/meals.js
+++ introduction-to-javascript/api-examples/js/meals.js
@@ -46,21 +46,23 @@ const loadMealDetails = (idMeal) => {
 
 }
 
-const displayMealDetaisl = meals => {
+const displayMealDetaisl = meal => {
     const detailedContainer = document.getElementById('detailed-container');
+    detailedContainer.innerHTML = ``;
     const mealDiv = document.createElement('div');
     mealDiv.classList.add('card');
     mealDiv.innerHTML = `
-    <img src="..." class="card-img-top" alt="...">
+    <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
     <div class="card-body">
-        <h5 class="card-title">Card title</h5>
+        <h5 class="card-title">${meal.strMeal}</h5>
         <p class="card-text">Some quick example text to build on the card title and make up the bulk of
             the card's content.</p>
         <a href="#" class="btn btn-primary">Go somewhere</a>
     </div>
    
-   
    `;
+    detailedContainer.appendChild(mealDiv);
+
 }
 
 loadMeals('');
