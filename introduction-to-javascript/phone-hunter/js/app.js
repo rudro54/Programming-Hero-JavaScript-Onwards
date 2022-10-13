5fb946dbc78897167d1df7e1de426eb3ff3bcb28
rudro54 <rudro54@gmail.com>
2022-10-13 21:31:04 +0600

    es6 intro added and working

--- introduction-to-javascript/phone-hunter/js/app.js
+++ introduction-to-javascript/phone-hunter/js/app.js
@@ -46,7 +46,9 @@ const displayPhones = (phones, dataLimit) => {
             <h5 class="card-title">${phone.phone_name}</h5>
             <p class="card-text">This is a longer card with supporting text below as a natural
             lead-in to additional content. This content is a little bit longer.</p>
-            <button onclick="loadPhoneDetails('${phone.slug}')" href="#" class = "btn btn-primary">Show Details</button>    
+            <button onclick="loadPhoneDetails('${phone.slug}')" href="#" class = "btn btn-primary" data-bs-toggle="modal" data-bs-target="#phoneDetailModal">Show Details</button>  
+            </button>
+
         </div>
         </div>
         
@@ -111,11 +113,18 @@ const loadPhoneDetails = async id => {
     const url = `https://openapi.programming-hero.com/api/phone/${id}`;
     const res = await fetch(url);
     const data = await res.json();
-    console.log(data.data);
+    displayPhoneDetail(data.data);
+}
+
+const displayPhoneDetail = phone => {
+    console.log(phone);
+    const modalTitle = document.getElementById('phoneDetailModalLabel');
+    modalTitle.innerText = phone.name;
+
 }
 
 
-//loadPhone(); // calling the function
+loadPhone('apple'); // calling the function
 
 
 
@@ -278,4 +287,5 @@ const loadPhoneDetails = async id => {
 // now check by clicking and inspecting the button if they recieve the slug value 
 // google search javascript input field enter key event handler
 // will use modal for show details
+// in case of modal remember one thing button trigger and 
 
