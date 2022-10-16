8aae2a39cc41c753c6fe68271f465809dbd0857d
rudro54 <rudro54@gmail.com>
2022-10-16 20:55:51 +0600

    es6 intro added and working

--- introduction-to-javascript/explore-js-object/object.js
+++ introduction-to-javascript/explore-js-object/object.js
@@ -1,4 +1,5 @@
 // create object using object literals
+// object have two things : properties and method
 
 const player = {};
 
@@ -9,5 +10,24 @@ player.bat = function () { // anynymous function as no name
     console.log('swinging the bat');
 };
 
-console.log(player);
-player.bat();
+//console.log(player);
+//player.bat();
+
+const student = {
+    name: 'panedey',
+    job: 'khay andey',
+    ball: function () {
+        console.log('throw the ball');
+
+    },
+    salary: 10000
+
+
+}
+
+//console.log(student);
+// using object  contructor 
+
+const person = new Object();
+console.log(person);
+
