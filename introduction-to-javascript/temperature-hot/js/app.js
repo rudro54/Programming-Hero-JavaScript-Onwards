e2211f3f58ef8514c7f68b66c0e8c7eae63929fb
rudro54 <rudro54@gmail.com>
2022-10-18 15:44:58 +0600

    hot weather started

--- introduction-to-javascript/temperature-hot/js/app.js
+++ introduction-to-javascript/temperature-hot/js/app.js
@@ -1,2 +1,10 @@
 // do not share your api key 
 const API_KEY = `9227ae1eb049f8671ebc84444ce38d8c`;
+
+// dyanamic 
+
+const loadTemperature = city => {
+    const url = 'https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}'
+}
+// you can add differenet parameters as well
+// &units = metric to show the way we are checking weather in dhaka
