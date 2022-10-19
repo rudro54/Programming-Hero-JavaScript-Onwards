00d99ff534828dd80c2dc89a041a227c77a15d7e
rudro54 <rudro54@gmail.com>
2022-10-19 20:23:33 +0600

    async studying

--- introduction-to-javascript/js-internals/async.js
+++ introduction-to-javascript/js-internals/async.js
@@ -5,6 +5,13 @@ function doSomething() {
 
 console.log('first');
 console.log('second');
-doSomething();
+//setTimeout(() => { console.log('third') }, 5000);
+fetch('https://jsonplaceholder.typicode.com/todos/1')
+    .then(response => {
+        console.log('response recieved');
+        return response.json()
+
+    })
+    .then(json => console.log(json))
 console.log('forth');
 console.log('fifth');
---
+++ introduction-to-javascript/js-internals/interval.js
@@ -0,0 +1,9 @@
+
+
+console.log('first');
+console.log('second');
+setInterval(() => {
+    console.log('third');
+}) // use control plus c to end the interval
+console.log('forth');
+console.log('fifth');
