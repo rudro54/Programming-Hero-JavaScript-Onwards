742d6ca7d30482101da7db5b5b16cf06bbcc5936
rudro54 <rudro54@gmail.com>
2022-10-21 17:59:08 +0600

    async studying

---
+++ introduction-to-javascript/js-internals/practice.js
@@ -0,0 +1,39 @@
+/*
+1. some middle console log will be showed after 3.5 seconds
+
+console.log("first showing ");
+setTimeout(() => {
+    console.log("second showing");
+
+
+}, 3500);
+
+console.log("third showing");
+
+2. take a number from user using prompt and add add 250 
+with that number and then display the number using alert
+
+const takeInput = () => {
+
+    const value = prompt("Give us a number");
+    const valueInNumber = parseInt(value);
+    const result = valueInNumber + 25;
+    alert(`your desired result is : ${result}`);
+}
+
+3. what is cookie ? write in english at least 5-7 lines 
+
+A cookie is an amount of information that persists between 
+a server-side and a client-side. A web browser stores this 
+information at the time of browsing. A cookie contains the 
+information as a string generally in the form of a name-value
+pair separated by semi-colons.
+
+
+4. write at leaset 3 differences between local storage 
+vs session storage 
+
+
+
+*/
+
