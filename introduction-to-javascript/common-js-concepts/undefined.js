c8a3342a659ebe31f4e2d9ce0b38f4a81628d2e4
rudro54 <rudro54@gmail.com>
2022-10-22 15:20:41 +0600

    async studying

--- introduction-to-javascript/common-js-concepts/undefined.js
+++ introduction-to-javascript/common-js-concepts/undefined.js
@@ -5,3 +5,36 @@ function second(a, b) {
 }
 const result = second();
 console.log(result);
+
+function third(a, b, c, d) {
+    const result = a + b + c + d;
+    console.log(a, b, c, d);
+}
+third(2, 5);
+
+function noNegative(a, b) {
+    if (a < 0 || b < 0) {
+        return
+    }
+    else {
+        return a + b;
+    }
+}
+let resultNoNegative = noNegative(5, -2);
+console.log(resultNoNegative);
+
+const fifth = { name: 'zayed', age: 43 };
+console.log(fifth.salary);
+
+const sixth = [89, 99, 0, 77, 98, 400];
+// you should not do it , instead use splice   
+delete sixth[1]; // deleting explicitely
+console.log(sixth[1], sixth[30], sixth[500]);
+console.log(sixth); // will create a hole here 
+const eight = undefined;
+// better way
+const nine = null;
+
+const data = { results: [], updated: null }; // null is an object
+console.log(typeof undefined);
+console.log(typeof null);
