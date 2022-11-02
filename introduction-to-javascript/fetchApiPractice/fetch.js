ba8f99bc37342d1dcbb110992bb888bf4ad20655
rudro54 <rudro54@gmail.com>
2022-11-02 13:34:30 +0600

    fetchApiLearning

--- introduction-to-javascript/fetchApiPractice/fetch.js
+++ introduction-to-javascript/fetchApiPractice/fetch.js
@@ -5,11 +5,15 @@ const myBtn = document.getElementById('myBtn');
 // div with id content
 const content = document.getElementById('content')
 
+//Get Request
 function getData() {
     console.log('started get data');
-    const url = `https://jsonplaceholder.typicode.com/users`;
+    const url = `https://api.github.com/users`;
     fetch(url)
-        .then(res => res.json())
+        .then(res => {
+            console.log('inside first then');
+            return res.json(); // we need in objects
+        })
         .then(data => {
             console.log('inside second then');
             console.log(data);
@@ -17,6 +21,34 @@ function getData() {
 
 }
 
-console.log('before running get data');
-getData();
-console.log('after running get data');
+// fetch of post takes url and parameters 
+function postData() {
+    // you have to stringify object
+    // fake post request api search google 
+    url = "http://dummy.restapiexample.com/api/v1/create";
+    // this is string so no need to stringify
+    data = {
+        "name": "Zayed5645",
+        "salary": 1000,
+        "age": 28
+    };
+
+
+    params = {
+        method: 'post',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data) //if data in string format 
+        // no need to stringify
+
+    }
+    fetch(url, params).then(res => res.json()).then(data => console.log('data'))
+
+
+}
+
+// console.log('before running get data');
+// getData();
+// console.log('after running get data');
+postData();
