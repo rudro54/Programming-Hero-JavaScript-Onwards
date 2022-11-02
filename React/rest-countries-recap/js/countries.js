08638a79cf76ff13900d8a20acbb28a56e5c0dc0
rudro54 <rudro54@gmail.com>
2022-11-02 20:15:12 +0600

    react learning

---
+++ React/rest-countries-recap/js/countries.js
@@ -0,0 +1,54 @@
+const loadCountries = () => {
+    fetch('https://restcountries.com/v3.1/all')
+        .then(res => res.json())
+        .then(data => displayCountries(data))
+
+}
+
+const displayCountries = countries => {
+    const countriesHTML = countries.map(country => getCountryHTML(country))
+    // console.log(countriesHTML[0]);
+    const container = document.getElementById('countries');
+    container.innerHTML = countriesHTML.join('');
+}
+
+const getCountryHTML = ({ name, flags }) => {
+    // option 3 
+    return `
+       <div class = "country">
+        <h2>${name.common}</h2>
+        <img src ="${flags.png}">
+       </div>
+
+    `
+}
+
+
+
+
+// const getCountryHTML = country => {
+// option 1
+
+//     return `
+//        <div class = "country">
+//         <h2>${country.name.common}</h2>
+//         <img src ="${country.flags.png}">
+//        </div>
+
+//     `
+// }
+
+// const getCountryHTML = country => {
+//     // option two
+//     const { name, flags } = country;
+
+//     return `
+//        <div class = "country">
+
+//         <h2>${name.common}</h2>
+//         <img src ="${flags.png}">
+//        </div>
+//     `
+// }
+
+loadCountries();
