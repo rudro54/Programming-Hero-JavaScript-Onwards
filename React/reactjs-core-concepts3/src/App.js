9e5ccbc23576f48fdb7082f5287e722e5c177707
rudro54 <rudro54@gmail.com>
2022-11-03 23:38:02 +0600

    react learning JSX3

--- React/reactjs-core-concepts3/src/App.js
+++ React/reactjs-core-concepts3/src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 function App() {
   return (
     <div className="App">
       <Counter></Counter>
+      <LoadComments></LoadComments>
     </div>
   );
 }
@@ -29,7 +30,40 @@ function Counter() {
 
 }
 
+function LoadComments() {
+  const [comments, setComments] = useState([]);
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/comments')
+      .then(res => res.json())
+      .then(data => setComments(data))
 
+
+
+  }, [])
+  return (
+    <div>
+      <h2>Comments Numbers :{comments.length}</h2>
+      {
+        comments.map(comment => <Comment email={comment.email} body={comment.body}></Comment>)
+      }
+
+    </div>
+
+
+  )
+
+
+}
+
+function Comment(props) {
+  return (
+    <div>
+      <h4>email : {props.email}</h4>
+      <p>{props.body}</p>
+    </div>
+
+  )
+}
 
 
 
