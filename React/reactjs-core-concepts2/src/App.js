494c2dd6d16ab850501571b42f5a6d4f66c5c9ca
rudro54 <rudro54@gmail.com>
2022-11-03 17:36:30 +0600

    react learning JSX2

--- React/reactjs-core-concepts2/src/App.js
+++ React/reactjs-core-concepts2/src/App.js
@@ -1,25 +1,64 @@
 import logo from './logo.svg';
 import './App.css';
 
+
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Counter></Counter>
     </div>
   );
 }
 
+function Counter() {
+  return (
+    <div>
+      <h1>Count : </h1>
+
+    </div>
+
+  );
+}
+
+
+
+
+
 export default App;
+
+
+/*
+const products = [
+  { name: 'laptop', price: '120,000' },
+  { name: 'iPhone', price: '200,000' },
+  { name: 'watch', price: '90,000' }
+
+]
+
+     <Product name="Laptop" price="12000"></Product>
+      <Product name="iPhone" price="200000"></Product>
+      <Product name="Watch" price="90000"></Product>
+      
+      {
+        products.map(product => <Product name={product.name} price={product.price}></Product>)
+
+      }
+      {}
+
+      function Product(props) {
+  return (
+
+    <div className='product'>
+      <h3>Name :{props.name}</h3>
+      <p>Price :{props.price}</p>
+    </div>
+
+  )
+
+
+}
+      
+      
+      
+      */
