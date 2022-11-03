4d1978a898740444637c01788950bf46e850b56b
rudro54 <rudro54@gmail.com>
2022-11-03 18:56:21 +0600

    react learning JSX2

--- React/reactjs-core-concepts2/src/App.js
+++ React/reactjs-core-concepts2/src/App.js
@@ -1,20 +1,50 @@
 import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 
 
 function App() {
   return (
     <div className="App">
-      <Counter></Counter>
+      {/* <Counter></Counter> */}
+      <ExternalUsers></ExternalUsers>
     </div>
   );
 }
 
+function ExternalUsers() {
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(res => res.json())
+      .then(data => setUsers(data))
+
+  }, [])
+  return (
+    <div>
+      <h2>External Users :</h2>
+      <p>{users.length}</p>
+      {
+        users.map(user => <User name={user.name} email={user.email}></User>)
+      }
+    </div>
+  );
+}
+
+function User(props) {
+  return (
+    <div style={{ border: '2px solid red', margin: '20px', backgroundColor: 'aqua' }}>
+      <h3>Name : {props.name}</h3>
+      <p>email : {props.email}</p>
+    </div >
+  )
+}
+
 function Counter() {
-  const [count, setCount] = useState(100);
+  const [count, setCount] = useState(0);
   const increaseCount = () => setCount(count + 1);
+  const decreaseCount = () => setCount(count - 1);
   // const increaseCount = () => {
   //   const newCount = count + 1;
   //   setCount(newCount);
@@ -24,6 +54,7 @@ function Counter() {
     <div>
       <h1>Count :{count} </h1>
       <button onClick={increaseCount}>Increase</button>
+      <button onClick={decreaseCount}>Decrease</button>
 
     </div>
 
