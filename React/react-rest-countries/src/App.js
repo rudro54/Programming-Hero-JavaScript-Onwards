1ee1eb8527100fb7201c651a4a487c263e051530
rudro54 <rudro54@gmail.com>
2022-11-04 13:40:25 +0600

    react-rest-countries

--- React/react-rest-countries/src/App.js
+++ React/react-rest-countries/src/App.js
@@ -1,6 +1,6 @@
-import logo from './logo.svg';
+
 import './App.css';
-import { useEffect, useState } from 'react';
+
 
 function App() {
   return (
@@ -11,36 +11,36 @@ function App() {
 }
 
 
-function LoadCountries() {
-  const [countries, setCountries] = useState([]);
-  useEffect(() => {
-    fetch('https://restcountries.com/v3.1/all')
-      .then(res => res.json())
-      .then(data => setCountries(data))
+// function LoadCountries() {
+//   const [countries, setCountries] = useState([]);
+//   useEffect(() => {
+//     fetch('https://restcountries.com/v3.1/all')
+//       .then(res => res.json())
+//       .then(data => setCountries(data))
 
-  }, [])
-  return (
-    <div>
-      <h1>Visiting Every Country Of The World!</h1>
-      <h3>Available Countries : {countries.length}</h3>
-      {
+//   }, [])
+//   return (
+//     <div>
+//       <h1>Visiting Every Country Of The World!</h1>
+//       <h3>Available Countries : {countries.length}</h3>
+//       {
 
-        countries.map(country => <Country name={country.name.common} population={country.population}></Country>)
+//         countries.map(country => <Country name={country.name.common} population={country.population}></Country>)
 
-      }
-    </div>
+//       }
+//     </div>
 
-  )
+//   )
 
-}
-function Country(props) {
-  return (
-    <div>
-      <h2>Name : {props.name}</h2>
-      <p>Population: {props.population}</p>
-    </div>
+// }
+// function Country(props) {
+//   return (
+//     <div>
+//       <h2>Name : {props.name}</h2>
+//       <p>Population: {props.population}</p>
+//     </div>
 
-  )
+//   )
 
-}
+// }
 export default App;
