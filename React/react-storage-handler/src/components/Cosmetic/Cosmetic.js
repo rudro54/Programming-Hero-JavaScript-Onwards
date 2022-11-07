fe96c1994acca61507e187534a6be820d9fc7f4f
rudro54 <rudro54@gmail.com>
2022-11-07 20:07:13 +0600

    reactjs-storage-handler

--- React/react-storage-handler/src/components/Cosmetic/Cosmetic.js
+++ React/react-storage-handler/src/components/Cosmetic/Cosmetic.js
@@ -1,13 +1,29 @@
 import React from 'react';
+import { addToDb } from '../../utilities/fakedb';
+import './Cosmetic.css'
 
 const Cosmetic = (props) => {
     const { name, id, price } = props.cosmetic;
+    const addToCart = (id) => {
+        addToDb(id);
+    }
+
+    const addToCartWithParam = () => addToCart(id);
 
     return (
-        <div>
+        <div className='product'>
             <h2>Buy This : {name} </h2>
             <p>Only for ${price}</p>
             <strong>id : {id}</strong>
+            <br />
+            <br />
+            <button onClick={addToCartWithParam}>Add To Cart</button>
+            <br />
+            <br />
+            <button onClick={() => addToCart(id)}>Add To Cart : shortcut</button>
+            <br />
+            <br />
+            <button onClick={() => addToCart(id)}>Purchase</button>
         </div>
     );
 };
