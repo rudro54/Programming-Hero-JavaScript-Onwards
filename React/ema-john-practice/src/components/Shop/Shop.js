43e5ae2ca3091f50aa53d725ebe2bc8234f68d55
rudro54 <rudro54@gmail.com>
2022-11-10 18:17:38 +0600

    copied product.json inside fake data folder which is under src then copy that into public folder so that it can be minified later

---
+++ React/ema-john-practice/src/components/Shop/Shop.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Shop.css';
+
+const Shop = () => {
+    return (
+        <div className='shop-container'>
+            <div className="products-container">
+                <h1>this is for product</h1>
+            </div>
+            <div className="cart-container">
+                <h1>order summary</h1>
+            </div>
+        </div>
+    );
+};
+// copy src>fakedata>product.json into public folder
+
+export default Shop;
