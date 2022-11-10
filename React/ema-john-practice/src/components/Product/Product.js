50ebeb3d0cc270be86d6ebcdebb682f54f9bbd28
rudro54 <rudro54@gmail.com>
2022-11-10 19:11:43 +0600

    always style will be aplied where i want show the data , indivial styles will be separate though

---
+++ React/ema-john-practice/src/components/Product/Product.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './Product.css'
+
+const Product = (props) => {
+    // destructuring so that we need not to write big names
+    const { name, img, seller, ratings } = props.product;
+    return (
+        <div className='product'>
+            <img src={img} alt=""></img>
+            <div className='product-info'>
+                <p>{name}</p>
+                <p>Manufacturer : {seller}</p>
+                <p>Ratings : {ratings}</p>
+            </div>
+            <button className='btn-cart'>
+                <p>Add To Cart</p>
+            </button>
+        </div>
+    );
+};
+
+export default Product;
--- React/ema-john-practice/src/components/Shop/Shop.js
+++ React/ema-john-practice/src/components/Shop/Shop.js
@@ -1,11 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Product from '../Product/Product';
 import './Shop.css';
 
 const Shop = () => {
+    //loading data use state 
+    const [products, setProducts] = useState([]);
+    // now need useState to fetch data
+    useEffect(() => {
+
+        fetch('products.json') // fetching from local file
+            .then(res => res.json())
+            .then(data => setProducts(data))
+    }, [])
+    // this last array is dependency injection 
+
     return (
         <div className='shop-container'>
             <div className="products-container">
-                <h1>this is for product</h1>
+                {
+                    products.map(product => <Product
+                        key={product.id}
+                        product={product}
+
+
+                    ></Product>)
+                }
             </div>
             <div className="cart-container">
                 <h1>order summary</h1>
