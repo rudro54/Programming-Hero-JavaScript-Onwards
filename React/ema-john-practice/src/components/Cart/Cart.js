d801f975260576b7c4e044354e5f38d203d8ca42
rudro54 <rudro54@gmail.com>
2022-11-10 22:36:19 +0600

    git componets created as cart

--- React/ema-john-practice/src/components/Cart/Cart.js
+++ React/ema-john-practice/src/components/Cart/Cart.js
@@ -1,10 +1,26 @@
 import React from 'react';
+import './Cart.css';
 
 const Cart = ({ cart }) => {
+    let total = 0; // as the value will be updated you can use reducer here
+    let shipping = 0;
+    for (const product of cart) {
+        total = total + product.price;
+        shipping = shipping + product.shipping;
+    }
+    const tax = parseFloat((total * 0.10).toFixed(2)); // used parseFloat
+    const grandTotal = total + shipping + tax;  // this will show a big number as 
+    // using toFixed  made the shipping a string
+
     return (
-        <div>
+        <div className='cart'>
             <h1>order summary</h1>
             <p>Selected Items : {cart.length}</p>
+            <p>Total Price : ${total}</p>
+            <p>Total Shipping: ${shipping}</p>
+            <p>Tax :{tax}</p>
+            <h5>Grand Total : ${grandTotal.toFixed(2)}</h5>
+
         </div>
     );
 };
