d1dca78a0b3660109002406e2a6f66d6dc0ccae2
rudro54 <rudro54@gmail.com>
2022-11-10 22:05:16 +0600

    git componets created as cart

--- React/ema-john-practice/src/components/Shop/Shop.js
+++ React/ema-john-practice/src/components/Shop/Shop.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
 import './Shop.css';
 
@@ -36,8 +37,7 @@ const Shop = () => {
                 }
             </div>
             <div className="cart-container">
-                <h1>order summary</h1>
-                <p>Selected Items : {cart.length}</p>
+                <Cart cart={cart}></Cart>
             </div>
         </div>
     );
