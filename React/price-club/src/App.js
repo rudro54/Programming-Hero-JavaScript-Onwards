f58afe2cf35fcd88647548e41785b46b9683d2dd
rudro54 <rudro54@gmail.com>
2022-11-15 13:22:57 +0600

    tailwind using in react app

---
+++ React/price-club/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className='text-4xl font-bold'>this is a big big header</h1>
+      <p>this is just a paragraph</p>
+    </div>
+  );
+}
+
+export default App;
