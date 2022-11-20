31954dfd35ceb17bd56ba40225f5683ddab54fe8
rudro54 <rudro54@gmail.com>
2022-11-21 02:55:07 +0600

    learning routes

---
+++ React/explore-router-comp/src/components/Products.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Products = () => {
+    return (
+        <div>
+            <h2>Lots of products are there</h2>
+        </div>
+    );
+};
+
+export default Products;
