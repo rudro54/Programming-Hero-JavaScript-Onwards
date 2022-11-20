97793f629c73db04842ff6b1ac8bdc57316dd16c
rudro54 <rudro54@gmail.com>
2022-11-20 20:19:10 +0600

    learning routes

---
+++ React/explore-routes/_steps.js
@@ -0,0 +1,18 @@
+/* 
+1. install react router npm i react-router-dom
+2. create a router using createBrowserRouter
+3. add RouterProvider and pass router props
+4. create some routes inside the router
+
+*/
+
+/*
+Things you need to create a route :
+1. create at Link : so that you can go to this route
+2. create component : to add what you will show once you go
+to that route
+3. add route so that react router knows the component
+it needs to display while you are visiting to that route
+
+
+*/
---
+++ React/explore-routes/src/components/About/About.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from '../Header/Header';
+import Home from '../Home/Home';
+
+const About = () => {
+    return (
+        <div>
+            <Header></Header>
+            <h2>This is real about </h2>
+        </div>
+    );
+};
+
+export default About;
