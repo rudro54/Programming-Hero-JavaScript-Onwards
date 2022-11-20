449485190ad1f1179db031da7519f594439a1589
rudro54 <rudro54@gmail.com>
2022-11-21 00:59:12 +0600

    learning routes

--- React/explore-routes/src/App.js
+++ React/explore-routes/src/App.js
@@ -9,6 +9,7 @@ import Friends from './components/Friends/Friends';
 import FriendDetails from './components/FriendDetails/FriendDetails';
 import Post from './components/Posts/Posts';
 import Posts from './components/Posts/Posts';
+import PostDetails from './components/PostDetails/PostDetails';
 
 function App() {
   const router = createBrowserRouter([
@@ -36,15 +37,18 @@ function App() {
           },
           { path: '/products', element: <Products></Products> },
           {
-
             path: 'posts',
             loader: async () => {
               return fetch('https://jsonplaceholder.typicode.com/posts')
             },
             element: <Posts></Posts>
-
-
-
+          },
+          {
+            path: '/post/:postId',
+            loader: async ({ params }) => {
+              return fetch(`https://jsonplaceholder.typicode.com/posts/${params.postId}`)
+            },
+            element: <PostDetails></PostDetails>
           }
 
 
