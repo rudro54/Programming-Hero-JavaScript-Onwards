6d6325ea829807b9d573c1ef56fde3b847e46ff4
rudro54 <rudro54@gmail.com>
2022-11-22 14:25:11 +0600

    t-shirt-mania

---
+++ React/t-shirt-mania/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+function App() {
+  const router = createBrowserRouter([
+
+
+  ])
+  return (
+    <div className="App">
+      <RouterProvider router={router}></RouterProvider>
+
+    </div>
+  );
+}
+
+export default App;
