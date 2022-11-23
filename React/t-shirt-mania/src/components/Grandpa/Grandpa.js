081782b8d9ce565a2a26cf5c337d875155dbce93
rudro54 <rudro54@gmail.com>
2022-11-23 13:49:29 +0600

    t-shirt-mania context done

--- React/t-shirt-mania/src/components/Grandpa/Grandpa.js
+++ React/t-shirt-mania/src/components/Grandpa/Grandpa.js
@@ -5,20 +5,24 @@ import Uncle from '../Uncle/Uncle';
 import './Grandpa.css'
 
 export const RingContext = createContext('Matir Ring');
+export const MoneyContext = createContext(555);
 
 const Grandpa = () => {
     const [house, setHouse] = useState(1);
+    const [money, setMoney] = useState(555);
     //const ring = "gold ring";
     return (
         <RingContext.Provider value={[house, setHouse]}>
-            <div className='grandpa'>
-                <h2>Grandpa</h2>
-                <section className='flex'>
-                    <Father house={house} ></Father>
-                    <Uncle house={house}></Uncle>
-                    <Aunt house={house}></Aunt>
-                </section>
-            </div>
+            <MoneyContext.Provider value={[money, setMoney]}>
+                <div className='grandpa'>
+                    <h2>Grandpa</h2>
+                    <section className='flex'>
+                        <Father house={house} ></Father>
+                        <Uncle house={house}></Uncle>
+                        <Aunt house={house}></Aunt>
+                    </section>
+                </div>
+            </MoneyContext.Provider>
         </RingContext.Provider>
     );
 };
