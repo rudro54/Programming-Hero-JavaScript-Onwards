31ef21212e676cf075557c70a94cd66d813d5673
rudro54 <rudro54@gmail.com>
2022-11-28 15:57:09 +0600

    iphone-hunter-server

---
+++ React/iphone-hunter-server/index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const port = 4000;
+const phones = require('./phones.json');
+
+app.get('/', (req, res) => {
+    res.send('Look Mama Kemne KI !! iPhone server is running');
+});
+
+app.get('/phones', (req, res) => {
+    res.send(phones);
+})
+
+app.get('/phones/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const phone = phones.find(ph => ph.id === id) || {};
+    res.send(phone);
+});
+
+app.listen(port, () => {
+
+    console.log(`iPhone server is running on port :${port}`);
+
+});
