1496bdb8d896b61ef033bcf5d3485fc1af705dd5
rudro54 <rudro54@gmail.com>
2022-11-28 22:46:06 +0600

    getting data from server using cors

--- React/iphone-hunter-client/src/App.js
+++ React/iphone-hunter-client/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Main from './layout/Main';
 import Home from './components/Home';
+import Phone from './components/Phone';
 
 function App() {
   const router = createBrowserRouter([
@@ -14,7 +15,10 @@ function App() {
           path: '/',
           element: <Home></Home>,
           loader: () => fetch('http://localhost:4000/phones')
-
+        },
+        {
+          path: '/phones/:id',
+          element: <Phone></Phone>
         }
 
 
--- React/iphone-hunter-client/src/components/Home.js
+++ React/iphone-hunter-client/src/components/Home.js
@@ -1,12 +1,15 @@
 import React from 'react';
-import { useLoaderData } from 'react-router-dom';
+import { Link, useLoaderData } from 'react-router-dom';
 
 const Home = () => {
-    const users = useLoaderData();
+    const phones = useLoaderData();
     return (
         <div>
             <h3>Show Me All The Phones You Have</h3>
-            <h5>I got :{users.length}</h5>
+            <h5>I got :{phones.length}</h5>
+            {
+                phones.map(ph => <li><Link to={`phones/${ph.id}`}>{ph.name}</Link></li>)
+            }
         </div>
     );
 };
